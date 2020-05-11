c4f4e11326e128ca065f993ea4a1429ff6efcd89
naka-gawa <tomoaki-nakagawa@c-fo.com>
2020-05-12 00:09:34 +0900

    add: slack function

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,77 @@
 import * as core from '@actions/core';
-import Docker from './docker';
+import { Slack, Success, Failure, Cancelled, Custom } from './slack';
+import { IncomingWebhookSendArguments } from '@slack/webhook';
 
 async function run(): Promise<void> {
   try {
-    // REGISTRY_NAME はユーザー側から渡せない様にする
-    const registry: string | undefined = process.env.REGISTRY_NAME
-    if (!registry) {
-      throw new Error('REGISTRY_NAME is not set.')
-    }
-    core.debug(registry)
-    if (process.env.GITHUB_TOKEN) {
-      core.setSecret(process.env.GITHUB_TOKEN)
-    }
+    const status = core.getInput('status', { required: true }).toLowerCase();
+    const mention = core.getInput('mention');
+    const author_name = core.getInput('author_name');
+    const if_mention = core.getInput('if_mention').toLowerCase();
+    const text = core.getInput('text');
+    const username = core.getInput('username');
+    const icon_emoji = core.getInput('icon_emoji');
+    const icon_url = core.getInput('icon_url');
+    const channel = core.getInput('channel');
+    const custom_payload = core.getInput('custom_payload');
+    const payload = core.getInput('payload');
+    const fields = core.getInput('fields');
 
-    const { IncomingWebhook } = require('@slack/webhook');
-    const url = process.env.SLACK_WEBHOOK_URL;
-    const webhook = new IncomingWebhook(url);
-    // Send the notification
-    await webhook.send({
-      text: 'I\'ve got news for you...',
-    });
+    core.debug(`status: ${status}`);
+    core.debug(`mention: ${mention}`);
+    core.debug(`author_name: ${author_name}`);
+    core.debug(`if_mention: ${if_mention}`);
+    core.debug(`text: ${text}`);
+    core.debug(`username: ${username}`);
+    core.debug(`icon_emoji: ${icon_emoji}`);
+    core.debug(`icon_url: ${icon_url}`);
+    core.debug(`channel: ${channel}`);
+    core.debug(`custom_payload: ${custom_payload}`);
+    core.debug(`payload: ${payload}`);
+    core.debug(`fields: ${fields}`);
+
+    const slack = new Slack(
+      {
+        status,
+        mention,
+        author_name,
+        if_mention,
+        username,
+        icon_emoji,
+        icon_url,
+        channel,
+        fields,
+      },
+      process.env.GITHUB_TOKEN,
+      process.env.SLACK_WEBHOOK_URL,
+    );
+
+    switch (status) {
+      case Success:
+        await slack.send(await slack.success(text));
+        break;
+      case Failure:
+        await slack.send(await slack.fail(text));
+        break;
+      case Cancelled:
+        await slack.send(await slack.cancel(text));
+        break;
+      case Custom:
+        /* eslint-disable no-var */
+        var evalPayload: IncomingWebhookSendArguments = eval(
+          `evalPayload = ${custom_payload}`,
+        );
+        /* eslint-enable */
+        await slack.send(evalPayload);
+        break;
+      default:
+        throw new Error(
+          'You can specify success or failure or cancelled or custom',
+        );
+    }
   } catch (error) {
-    core.error(error.toString())
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
